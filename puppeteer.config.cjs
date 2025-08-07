ae7a5e15e8d97820fac0957975343ede17e2e1e4
Md Samsad Alam <91345589+Samsadprince134@users.noreply.github.com>
2025-08-07 21:54:21 +0530

    Add files via upload

---
+++ puppeteer.config.cjs
@@ -0,0 +1,9 @@
+// puppeteer.config.cjs
+const { join } = require('path');
+
+/**
+ * @type {import("puppeteer").Configuration}
+ */
+module.exports = {
+  cacheDirectory: process.env.PUPPETEER_CACHE_DIR || join(__dirname, '.cache', 'puppeteer'),
+};
